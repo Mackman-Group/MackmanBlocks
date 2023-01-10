938d0f7aced9618528ad5fbc6c59694ebcd2b203
Adam Chamberlin <adam@mackman.co.uk>
2023-01-10 13:38:48 +0000

    bringing in Steeve’s work

--- patterns/hero-offset-hero-right.php
+++ patterns/hero-offset-hero-right.php
@@ -1,18 +1,15 @@
 <?php
 /**
- * Title: Offset Hero.
- * Slug: mackman/hero-offset-hero
+ * Title: Offset Hero - Text Left Image Right.
+ * Slug: mackman/hero-offset-hero-right
  * Categories: hero
  */
 ?>
 <!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"0px"},"padding":{"top":"var:preset|spacing|x-large","right":"30px","bottom":"var:preset|spacing|x-large","left":"30px"}}},"layout":{"type":"constrained"}} -->
   <div class="wp-block-group alignfull" style="margin-top:0px;padding-top:var(--wp--preset--spacing--x-large);padding-right:30px;padding-bottom:var(--wp--preset--spacing--x-large);padding-left:30px">
     
-    <!-- wp:media-text {"mediaId":277,"mediaLink":"http://mackman-group-block-test-site.test/wp-content/themes/mackman/assets/images/holding.jpg","mediaType":"image"} -->
-      <div class="wp-block-media-text alignwide is-stacked-on-mobile">
-        <figure class="wp-block-media-text__media">
-          <img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/holding.jpg'; ?>" alt="Sample Image" class="wp-image-277 size-full"/>
-        </figure>
+    <!-- wp:media-text {"mediaPosition":"right","mediaId":277,"mediaLink":"http://mackman-group-block-test-site.test/wp-content/themes/mackman/assets/images/holding.jpg","mediaType":"image"} -->
+      <div class="wp-block-media-text alignwide has-media-on-the-right is-stacked-on-mobile">
         <div class="wp-block-media-text__content">
           
           <!-- wp:group {"textColor":"neutral","className":"pull-left","layout":{"type":"constrained"}} -->
@@ -30,6 +27,9 @@
           <!-- /wp:group -->
             
         </div>
+        <figure class="wp-block-media-text__media">
+          <img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/holding.jpg'; ?>" alt="" class="wp-image-277 size-full"/>
+        </figure>
       </div>
     <!-- /wp:media-text -->
       
---
+++ patterns/text-tile-two.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Title: Tile in 2 columns
+ * Slug: mackman/text-tile-two
+ * Categories: text
+ * Viewport Width: 1280
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"0px"},"padding":{"top":"var:preset|spacing|x-large","right":"30px","bottom":"var:preset|spacing|x-large","left":"30px"}}},"className":"has-background-color","layout":{"type":"constrained"}} -->
+  <div class="wp-block-group alignfull has-background-color" style="margin-top:0px;padding-top:var(--wp--preset--spacing--x-large);padding-right:30px;padding-bottom:var(--wp--preset--spacing--x-large);padding-left:30px">
+  
+    <!-- wp:columns -->
+      <div class="wp-block-columns">
+        
+        <!-- wp:column {"width":"50%"} -->
+          <div class="wp-block-column" style="flex-basis:50%">
+            
+            <!-- wp:media-text {"mediaId":277,"mediaLink":"http://mackman-group-block-test-site.test/wp-content/themes/mackman/assets/images/holding.jpg","mediaType":"image","className":"stack"} -->
+              <div class="wp-block-media-text alignwide is-stacked-on-mobile stack">
+                <figure class="wp-block-media-text__media">
+                  <img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/holding.jpg'; ?>" alt="" class="wp-image-277 size-full"/>
+                </figure>
+                <div class="wp-block-media-text__content">
+                  
+                  <!-- wp:heading {"level":3} -->
+                    <h3>Heading</h3>
+                  <!-- /wp:heading -->
+                  
+                  <!-- wp:paragraph -->
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut etiam sit amet nisl purus in mollis. Tristique senectus et netus et malesuada fames.</p>
+                  <!-- /wp:paragraph -->
+                  
+                  <!-- wp:buttons -->
+                    <div class="wp-block-buttons">
+                      
+                      <!-- wp:button {"className":"btn-underline"} -->
+                        <div class="wp-block-button btn-underline">
+                          <a class="wp-block-button__link wp-element-button">Click Here</a>
+                        </div>
+                      <!-- /wp:button -->
+                        
+                    </div>
+                  <!-- /wp:buttons -->
+                    
+                </div>
+              </div>
+            <!-- /wp:media-text -->
+              
+          </div>
+        <!-- /wp:column -->
+        
+        <!-- wp:column {"width":"50%"} -->
+          <div class="wp-block-column" style="flex-basis:50%">
+            
+            <!-- wp:media-text {"mediaId":277,"mediaLink":"http://mackman-group-block-test-site.test/wp-content/themes/mackman/assets/images/holding.jpg","mediaType":"image","className":"stack"} -->
+              <div class="wp-block-media-text alignwide is-stacked-on-mobile stack">
+                <figure class="wp-block-media-text__media">
+                  <img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/holding.jpg'; ?>" alt="" class="wp-image-277 size-full"/>
+                </figure>
+                <div class="wp-block-media-text__content">
+                  
+                  <!-- wp:heading {"level":3} -->
+                    <h3>Heading</h3>
+                  <!-- /wp:heading -->
+                  
+                  <!-- wp:paragraph -->
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut etiam sit amet nisl purus in mollis. Tristique senectus et netus et malesuada fames.</p>
+                  <!-- /wp:paragraph -->
+                  
+                  <!-- wp:buttons -->
+                    <div class="wp-block-buttons">
+                      
+                      <!-- wp:button {"className":"btn-underline"} -->
+                        <div class="wp-block-button btn-underline">
+                          <a class="wp-block-button__link wp-element-button">Click Here</a>
+                        </div>
+                      <!-- /wp:button -->
+                        
+                    </div>
+                  <!-- /wp:buttons -->
+                    
+                </div>
+              </div>
+            <!-- /wp:media-text -->
+              
+          </div>
+        <!-- /wp:column -->
+        
+      </div>
+    <!-- /wp:columns -->
+  
+  </div>
+<!-- /wp:group -->
